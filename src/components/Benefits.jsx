65570bea537a3f85fa25a774511603a491c9c828
SuhasK2005 <saisuhask2005@gmail.com>
2025-10-19 13:08:37 +0530

    Removed the companylogo

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Benefits = () => {
-  return <div>Benefits</div>;
+  return <sections>Benefits</sections>
 };
 
 export default Benefits;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -93,7 +93,6 @@ const Hero = () => {
           <BackgroundCircles />
         </div>
 
-        <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
       </div>
 
       <BottomLine />
