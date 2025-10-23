7b9d4fe292db4011b05a64b22b97937a27cd9764
SuhasK2005 <saisuhask2005@gmail.com>
2025-10-23 17:41:20 +0530

    completed collaboration file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,11 @@
-import { brainwave } from "../assets";
-import { disablePageScroll, enablePageScroll } from "scroll-lock";
-import { navigation } from "../constants";
 import { useLocation } from "react-router-dom";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
+import { brainwave } from "../assets";
+import { navigation } from "../constants";
 import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { HamburgerMenu } from "./design/Header";
 import { useState } from "react";
-import ButtonSvg from "../assets/svg/ButtonSvg";
 
 const Header = () => {
   const pathname = useLocation();
@@ -23,20 +22,19 @@ const Header = () => {
   };
 
   const handleClick = () => {
-    if(!openNavigation) return;
-    enablePageScroll()
+    if (!openNavigation) return;
+
+    enablePageScroll();
     setOpenNavigation(false);
   };
 
   return (
     <div
-      className="fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 
-    lg:backdrop-blur-sm ${openNavigation ? 'bg-n-8': 'bg-n-8/90 backdrop-blur-sm'}"
+      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+      }`}
     >
-      <div
-        className="flex items-center px-5 lg:px-7.5
-        xl:px-10 max-lg:py-4"
-      >
+      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="Brainwave" />
         </a>
@@ -46,10 +44,7 @@ const Header = () => {
             openNavigation ? "flex" : "hidden"
           } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
         >
-          <div
-            className="relative z-2 flex flex-col items-center
-            justify-center m-auto lg:flex-row"
-          >
+          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
               <a
                 key={item.id}
@@ -63,13 +58,14 @@ const Header = () => {
                     : "lg:text-n-1/50"
                 } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
               >
-                {" "}
                 {item.title}
               </a>
             ))}
           </div>
+
           <HamburgerMenu />
         </nav>
+
         <a
           href="#signup"
           className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
